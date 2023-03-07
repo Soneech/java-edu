e4b1873dfd7ea905a07c8b11f6a52da20e3558ce
Soneech <sonya.patrina@yandex.ru>
2023-03-07 19:19:52 +0300

    initial commit

---
+++ app/src/main/java/org/mirea/javaedu/MainActivity.java
@@ -0,0 +1,14 @@
+package org.mirea.javaedu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
